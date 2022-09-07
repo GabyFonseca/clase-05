70f5145e915c271664f8193f28d459a2580d9ec8
GabyFonseca <g.fonsecar@uniandes.edu.co>
2022-09-07 18:03:25 -0500

    Caracteristicas de Datos

--- Script clase 5.R
+++ Script clase 5.R
@@ -25,6 +25,33 @@ export(x=data_csv, file="output/data_csv.csv")
 ##    Exportar a un formato .xls
 export(x=data_xls , file="output/data_excel.xlsx")
 
+## Indiquemos en que formato se debe de exportar .rds, .xlsx, …
+export(x=data_xls , file="output/data_r.rds")
 
 ##Convertir Bases de datos
 convert(in_file = "input/datos_censo.csv", out_file = "output/datos_convert.rds")
+
+convert(in_file = "input/datos_excel.xlsx", out_file = "output/datas_convertidos.rds")
+
+##EXPLORAR CONJUNTO DE DATOS Y SUS CARACTERISTICAS
+##Buscar base de datos que vienen con r
+data(package="dplyr")
+
+##Obtener Datos
+db = as_tibble(x = starwars)
+
+## pinta sobre la consola las primeras 5 observaciones de "db"
+head(x = db , n = 5) 
+
+## pinta sobre la consola las ultimas 5 observaciones de "db"
+tail(db) 
+
+## obtener la estructura del objeto "mtcars"
+str(object = mtcars) 
+
+## obtener la estructura del objeto "db"
+glimpse(x = db) 
+
+## de la librería skimr, resumir la base de datos
+skim(data = db) 
+
